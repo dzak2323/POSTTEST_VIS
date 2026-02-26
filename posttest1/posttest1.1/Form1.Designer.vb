9f5ca181ef6f2e0904f67ec6066ecd55a2bbe9eb
dzak2323 <23dzakie@gmail.com>
2026-02-27 01:54:23 +0800

    Add files via upload

---
+++ posttest1/posttest1.1/Form1.Designer.vb
@@ -0,0 +1,129 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        LabelIPK = New Label()
+        LabelPredikat = New Label()
+        TextBoxIPS = New TextBox()
+        ButtonTambah = New Button()
+        ButtonReset = New Button()
+        LabelSem = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Yu Gothic UI", 24F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Label1.Location = New Point(368, 78)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(300, 54)
+        Label1.TabIndex = 0
+        Label1.Text = "Penghitung IPK"
+        ' 
+        ' LabelIPK
+        ' 
+        LabelIPK.AutoSize = True
+        LabelIPK.BackColor = Color.White
+        LabelIPK.ForeColor = SystemColors.ActiveCaptionText
+        LabelIPK.Location = New Point(349, 340)
+        LabelIPK.Name = "LabelIPK"
+        LabelIPK.Size = New Size(37, 20)
+        LabelIPK.TabIndex = 1
+        LabelIPK.Text = "IPK: "
+        ' 
+        ' LabelPredikat
+        ' 
+        LabelPredikat.AutoSize = True
+        LabelPredikat.BackColor = Color.White
+        LabelPredikat.ForeColor = SystemColors.ActiveCaptionText
+        LabelPredikat.Location = New Point(615, 340)
+        LabelPredikat.Name = "LabelPredikat"
+        LabelPredikat.Size = New Size(63, 20)
+        LabelPredikat.TabIndex = 2
+        LabelPredikat.Text = "Predikat"
+        ' 
+        ' TextBoxIPS
+        ' 
+        TextBoxIPS.Location = New Point(441, 209)
+        TextBoxIPS.Name = "TextBoxIPS"
+        TextBoxIPS.Size = New Size(125, 27)
+        TextBoxIPS.TabIndex = 3
+        ' 
+        ' ButtonTambah
+        ' 
+        ButtonTambah.Location = New Point(349, 279)
+        ButtonTambah.Name = "ButtonTambah"
+        ButtonTambah.Size = New Size(94, 29)
+        ButtonTambah.TabIndex = 4
+        ButtonTambah.Text = "tambah"
+        ButtonTambah.UseVisualStyleBackColor = True
+        ' 
+        ' ButtonReset
+        ' 
+        ButtonReset.Location = New Point(574, 279)
+        ButtonReset.Name = "ButtonReset"
+        ButtonReset.Size = New Size(94, 29)
+        ButtonReset.TabIndex = 5
+        ButtonReset.Text = "Reset"
+        ButtonReset.UseVisualStyleBackColor = True
+        ' 
+        ' LabelSem
+        ' 
+        LabelSem.AutoSize = True
+        LabelSem.BackColor = Color.White
+        LabelSem.ForeColor = SystemColors.ActiveCaptionText
+        LabelSem.Location = New Point(476, 363)
+        LabelSem.Name = "LabelSem"
+        LabelSem.Size = New Size(99, 20)
+        LabelSem.TabIndex = 6
+        LabelSem.Text = "SemSekarang"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
+        ClientSize = New Size(1159, 666)
+        Controls.Add(LabelSem)
+        Controls.Add(ButtonReset)
+        Controls.Add(ButtonTambah)
+        Controls.Add(TextBoxIPS)
+        Controls.Add(LabelPredikat)
+        Controls.Add(LabelIPK)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents LabelIPK As Label
+    Friend WithEvents LabelPredikat As Label
+    Friend WithEvents TextBoxIPS As TextBox
+    Friend WithEvents ButtonTambah As Button
+    Friend WithEvents ButtonReset As Button
+    Friend WithEvents LabelSem As Label
+
+End Class
---
+++ posttest1/posttest1.1/Form1.vb
@@ -0,0 +1,76 @@
+﻿Public Class Form1
+
+    Dim totalIPS As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub ButtonTambah_Click(sender As Object, e As EventArgs) Handles ButtonTambah.Click
+        Dim ipsBaru As Double
+
+
+        If Double.TryParse(TextBoxIPS.Text, ipsBaru) Then
+
+
+            If ipsBaru >= 0 AndAlso ipsBaru <= 4.0 Then
+
+                totalIPS += ipsBaru
+                jumlahSemester += 1
+
+
+                Dim ipk As Double = totalIPS / jumlahSemester
+                LabelIPK.Text = "IPK = " & ipk.ToString("F2")
+                LabelSem.Text = "Semeseter sekarang = " & jumlahSemester
+
+
+
+
+                If ipk >= 3.01 Then
+                    LabelPredikat.Text = "Predikat: Sangat Memuaskan"
+                ElseIf ipk >= 2.76 Then
+                    LabelPredikat.Text = "Predikat: Memuaskan"
+                ElseIf ipk >= 2.0 Then
+                    LabelPredikat.Text = "Predikat: Cukup"
+                Else
+                    LabelPredikat.Text = "Predikat: Kurang"
+                End If
+
+
+                TextBoxIPS.Clear()
+                TextBoxIPS.Focus()
+            Else
+                MessageBox.Show("Masukkan nilai IP Semester antara 0.00 hingga 4.00!", "Peringatan", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            End If
+        Else
+            MessageBox.Show("tolong masukan angka", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub ButtonReset_Click(sender As Object, e As EventArgs) Handles ButtonReset.Click
+
+        totalIPS = 0
+        jumlahSemester = 0
+
+
+        LabelIPK.Text = "IPK: 0.00"
+        LabelPredikat.Text = "Predikat: -"
+
+
+        TextBoxIPS.Clear()
+        TextBoxIPS.Focus()
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub LabelIPK_Click(sender As Object, e As EventArgs) Handles LabelIPK.Click
+
+    End Sub
+
+    Private Sub TextBoxIPS_TextChanged(sender As Object, e As EventArgs) Handles TextBoxIPS.TextChanged
+
+    End Sub
+End Class
